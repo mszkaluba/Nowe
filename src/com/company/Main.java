dc626bdb0534a9584c2f75f128ec9daf4c1ffc80
Marcin <mszkaluba@o2.pl>
2020-07-13 17:27:44 +0200

    Add Mian.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,6 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        System.out.println("Cześć");
+        System.out.println("Cześć to ja");
+        System.out.println("Marcin");
     }
 }
